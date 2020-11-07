ab4873caae07c7d6b24163e594aa2de950080088
Alejandro Millan <alejandro_millan@ymail.com>
2020-11-06 20:34:50 -0800

    Get bytes from file and put in vector

--- main.cpp
+++ main.cpp
@@ -1,8 +1,42 @@
 #include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+vector<unsigned char> readFile(const string& fileName) {
+    ifstream inputFile;
+    inputFile.open(fileName, ios::binary);
+
+    if (!inputFile.is_open()) {
+        cout << "Error Opening File" << endl;
+        exit(0);
+    }
+
+    vector<unsigned char>bytes((istreambuf_iterator<char>(inputFile)), {});
+
+    inputFile.close();
+    return bytes;
+}
+
+void pasteDemo(const string& name, const vector<unsigned char>& fileBytes) {
+    ofstream output( name, ios::binary);
+
+    for(unsigned char fileByte : fileBytes) {
+        output.put(fileByte);
+    }
+
+    output.close();
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    string name = "hello.txt";
+    vector<unsigned char> fileBytes(readFile(name));
 
+    pasteDemo(name, fileBytes);
 
+    cout << "Hello, World!" << endl;
     return 0;
 }
